8ffd58155ff5284c2500d6f3886112f4cb2e2d7b
DoUrA2Sp5Am4P <113227954+DoUrA2Sp5Am4P@users.noreply.github.com>
2024-10-30 23:33:58 +0500

    Add files via upload

--- generator.py
+++ generator.py
@@ -2,13 +2,23 @@ from TTS.tts.configs.xtts_config import XttsConfig
 from TTS.tts.models.xtts import Xtts
 import soundfile as sf
 import time
+import argparse
+parser = argparse.ArgumentParser(
+                    prog='Medicine',
+                    description='Medicine',
+                    epilog='Medicine')
+parser.add_argument('-t', '--text') #Важно указывать аргументы
+parser.add_argument('-o','--output')
+args = parser.parse_args()
+TEXT=args.text
+OUTPUT=args.output
 config = XttsConfig()
 config.load_json("./XTTS-v2/config.json")
 model = Xtts.init_from_config(config)
 model.load_checkpoint(config, checkpoint_dir="./XTTS-v2/", eval=True)
 start_time = time.time()
 outputs = model.synthesize(
-    "Таблетка не найдена в базе",
+    TEXT,
     config,
     speaker_wav="./XTTS-v2/samples/en_sample.wav",
     gpt_cond_len=3,
@@ -16,7 +26,7 @@ outputs = model.synthesize(
 )
 end_time = time.time() 
 execution_time = end_time - start_time
-output_path = "./main/not_found.wav"
+output_path = OUTPUT
 sf.write(output_path, outputs['wav'], 22050)
 print(f"Файл сохранен как {output_path}")
 print(execution_time)
